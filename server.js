c12be45dde006f7f133648d3552cce6381db6da3
Ahmedarafat1 <43320775+Ahmedarafat1@users.noreply.github.com>
2020-11-18 01:49:32 +0200

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-    res.send('Hello World Hegazi');
+    res.send('Hello World Hegazi1');
 });
 
 app.listen(PORT, HOST);
